a1288a5ab332ba29379de82d77867e439012cf45
Aley2321 <2d.leonaraizayaelalexander@gmail.com>
2025-05-16 00:06:30 -0600

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    alert('Bienvenido a mi pagina informativa del cietifico, filosofo y alqimista PARACELSO. Mi nombre es Yael Alexander');
+});
